5e815251032a6fb48c91280eea14bf293fc43047
Athistaraja <athisraja45@gmail.com>
2025-02-24 22:14:04 +0530

    updates chatbot

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new Server(server, {
+  path: "/socket.io/",
   cors: {
     origin: "https://athistaraja-chatbot.netlify.app/",
     methods: ["GET", "POST"],
   },
 });
+
 
 app.use(cors());
 app.use(express.json());
